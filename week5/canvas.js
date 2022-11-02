75737f820c7e8c87a68f1f822c7a11491975f8a5
Nazier999 <njlacy@email.neit.edu>
2022-11-02 11:19:45 -0400

    added lab 5

---
+++ week5/canvas.js
@@ -0,0 +1,69 @@
+//this is a comment in JavaScript (line comment)
+/*
+Multiline JavaScript comment
+ */
+//defines variable to access the canvas properties
+var canvas = document.getElementById("canvas");
+//Defines the drawing context of the canvas
+var ctx = canvas.getContext("2d");
+
+//Draw a rectangle
+
+//defines the fill color
+ctx.fillStyle = "rgb(0,0,255)";
+//define outline color
+ctx.strokeStyle = "green";
+//defines line width
+ctx.lineWidth = "5";
+
+//finally draw rectangle fillrect(x,y,width,height)
+ctx.fillRect(30,30,200,100);
+ctx.strokeRect(300,30,200,100);
+
+//draw a line
+//draw MONSTO CARDO
+ctx.beginPath();
+ctx.moveTo(50,0);
+ctx.lineTo(400,300);
+ctx.lineTo(800,0);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red"
+ctx.moveTo(800,600)
+ctx.lineTo(400,350)
+ctx.lineTo(0,600)
+ctx.stroke()
+
+//Draw a Circle
+
+ctx.fillStyle = "#55ddef";
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = "2";
+
+ctx.beginPath()
+ctx.arc(400,300,50, 0, (3 * Math.PI)/2, false);
+ctx.lineTo(400,300)
+ctx.closePath();
+ctx.fill();
+ctx.stroke()
+
+//draw a shape
+ctx.beginPath()
+ctx.moveTo(650, 100);
+ctx.lineTo(700, 140);
+ctx.lineTo(675, 200);
+ctx.lineTo(625, 200);
+ctx.lineTo(600, 140);
+ctx.closePath();
+ctx.stroke();
+ctx.fill();
+
+//draw image to canvas
+//create instance  of image
+var mario = new Image();
+mario.src = "images/Mario.png"
+
+mario.onload = function(){
+    ctx.drawImage(mario, 60, 30, 400, 200)
+}
