aaf49e7aa3cf4d36b49162344aa737d68c418b90
Nazier999 <njlacy@email.neit.edu>
2022-11-29 11:26:44 -0500

    homework progress

--- week7/day2/rps.js
+++ week7/day2/rps.js
@@ -16,12 +16,42 @@ var vegeta = new Image();
 
 
 
+
+
 //utility function
 function randomNumber(high,low){
     return Math.round(Math.random() * (high - low) + low);
 }
 
+var startHealth = 100;//randomNumber(canvas.width, 600);
+var gokuhealth = startHealth;
+var vegetahealth = startHealth;
+var healthBarWidth = 300;
 
+function drawHealthBar(){
+    //draw fuel HUD
+    var gokucurrentBarWidth = healthBarWidth * (gokuhealth/startHealth);
+    var vegetacurrentBarWidth = healthBarWidth * (vegetahealth/startHealth);
+    ctx.save();
+    ctx.fillStyle = 'Black'
+    ctx.fillRect(70,30,healthBarWidth,10)
+    ctx.fillRect(670,30,healthBarWidth,10)
+    
+    if(gokuhealth>0){
+        ctx.fillStyle = "green";
+        ctx.font = "25px Arial";
+        ctx.fillText("hp", 50,40)
+        ctx.fillRect(70,30, gokucurrentBarWidth, 10);
+        
+    }
+    if(vegetahealth>0){
+        ctx.fillStyle = "green";
+        ctx.font = "25px Arial";
+        ctx.fillText("hp", 650,40)
+        ctx.fillRect(670,30, vegetacurrentBarWidth, 10);
+    }
+    ctx.restore();
+}
 
 rock.src = "images/rock.jpg";
 paper.src = "images/paper.jpg";
@@ -47,6 +77,10 @@ var gameOver = true;
 
 function keyPressDown(e){
     console.log(e.keyCode)
+    if(e.keycode == 32)
+    if (gokuhealth || vegetahealth <= 0) {
+        restartGame();
+    }
 }
 
     function keyPressUp(e){
@@ -87,7 +121,8 @@ function draw(rock, paper, scissors, crock, cpaper, cscissors, goku, vegeta){
         ctx.drawImage(cpaper, canvas.width/2 - paper.width/2, 375);
         ctx.drawImage(cscissors, canvas.width/2 - scissors.width/2 + 100, 375);
 
-        ctx.fillText(result, canvas.width/2, 525)
+        ctx.fillText(result, canvas.width/2, 525);
+        drawHealthBar();
 }
 
 //var canvas = document.getElementById("c");
@@ -148,17 +183,24 @@ function playGame(playerChoice) {
                 //itsa tie
                 //alert("CPU chose Rock. It's a tie!")
                 result = "CPU chose rock. its a tie"
-                draw(hrock,paper,scissors,hrock,paper,scissors)
+                vegetahealth -= 10;
+                gokuhealth -= 10;
+                draw(hrock,paper,scissors,hrock,paper,scissors,goku,vegeta)
+                
             }
             else if (cpuChoice == 1) {
                 //alert("CPU cose paper. CPU wins!");
-                result = "CPU chose paper. CPU wins"
-                draw(rock,hpaper,scissors,rock,hpaper,scissors)
+                result = "CPU chose paper. CPU wins";
+                gokuhealth -= 20;
+                draw(hrock,paper,scissors,rock,hpaper,scissors,goku,vegeta)
+                
             }
             else {
                 //alert("CPU chose Scissors. You win!")
-                result = "CPU chose rock. You Win!"
-                draw(rock,paper,hscissors,rock,paper,hscissors)
+                result = "CPU chose rock. You Win!";
+                vegetahealth -= 20;
+                draw(hrock,paper,scissors,rock,paper,hscissors,goku,vegeta)
+                
             }
             break;
         case "Paper":
@@ -166,17 +208,21 @@ function playGame(playerChoice) {
                 //itsa tie
                 //alert("CPU chose Rock. You Win!")
                 result = "CPU chose rock. You Win!"
-                draw(hrock,paper,scissors,hrock,paper,scissors)
+                vegetahealth -= 20;
+                draw(rock,hpaper,scissors,hrock,paper,scissors,goku,vegeta)
             }
             else if (cpuChoice == 1) {
                 //alert("CPU cose paper. It's a tie!");
                 result = "CPU chose paper. It's a tie!"
-                draw(rock,hpaper,scissors,rock,hpaper,scissors)
+                vegetahealth -= 10;
+                gokuhealth -= 10;
+                draw(rock,hpaper,scissors,rock,hpaper,scissors,goku,vegeta)
             }
             else {
                 //alert("CPU chose Scissors. You Lose!")
                 result = "CPU chose scissors. CPU Wins!"
-                draw(rock,paper,hscissors,rock,paper,hscissors)
+                gokuhealth -= 20;
+                draw(rock,hpaper,scissors,rock,paper,hscissors,goku,vegeta)
             }
             break;
         case "Scissors":
@@ -184,20 +230,27 @@ function playGame(playerChoice) {
                 //itsa tie
                 //alert("CPU chose Rock. You Lose!")
                 result = "CPU chose scissors. CPU Wins!"
-                draw(hrock,paper,scissors,hrock,paper,scissors,goku,vegeta)
+                gokuhealth -= 20;
+                draw(rock,paper,hscissors,hrock,paper,scissors,goku,vegeta)
             }
             else if (cpuChoice == 1) {
                 //alert("CPU cose paper. It's a Win!");
                 result = "CPU chose paper. You Win!"
-                draw(rock,hpaper,scissors,rock,hpaper,scissors)
+                vegetahealth -= 20;
+                draw(rock,paper,hscissors,rock,hpaper,scissors,goku,vegeta)
             }
             else {
                // alert("CPU chose Scissors. It's a Tie!")
                result = "CPU chose scissors. It's a Tie!"
-               draw(rock,paper,hscissors,rock,paper,hscissors)
+               vegetahealth -= 10;
+                gokuhealth -= 10;
+               draw(rock,paper,hscissors,rock,paper,hscissors,goku,vegeta)
             }
             break;
 
     }
 }
 
+function restartGame(){
+    location.reload();
+ }
