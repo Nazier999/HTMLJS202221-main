9c9eb5997c51ae43176183142151749a1d8fbcd4
Nazier999 <njlacy@email.neit.edu>
2022-12-06 08:01:02 -0500

    m

--- week8/Lab/canvas.js
+++ week8/Lab/canvas.js
@@ -17,7 +17,7 @@ var seconds = 3;
 var fps = 60;
 var frames = fps;
 
-<<<<<<< HEAD:week8/canvas.js
+
 //terrain and car
 var van = new Image();
 var road = new Image();
@@ -25,7 +25,7 @@ var road = new Image();
 //terrain and car src
 van.src = "images/Van.png"
 road.src = "images/DesertRoad.jpg"
-=======
+
 //car sprite
 var car= new Image();
 car.src = "images/Car.png";
@@ -34,7 +34,7 @@ car.onload = function(){
     main();
 }
 var carWidth = 50;
->>>>>>> 9a69eb7 (stuff):week8/Lab/canvas.js
+
 
 //addEventListener
 document.addEventListener('keydown', pressSpace)
@@ -89,13 +89,13 @@ function main() {
 
         drawFuelBar();
 
-<<<<<<< HEAD:week8/canvas.js
+
        
         
         if (fuel <= 0 || carPos + 40 > finish) {
-=======
+
         if (fuel <= 0 || carPos + carWidth > finish) {
->>>>>>> 9a69eb7 (stuff):week8/Lab/canvas.js
+
             drawResults();
 
         }
@@ -170,12 +170,11 @@ function drawStartFinsihLines() {
 
 function drawCar() {
     //ctx.fillStyle = 'red';
-<<<<<<< HEAD:week8/canvas.js
+
    // ctx.fillRect(carPos, canvas.height / 2, 40, 20);
-=======
+
     //ctx.fillRect(carPos, canvas.height / 2, 40, 20);
     ctx.drawImage(car, carPos, canvas.height/2, 50, 20);
->>>>>>> 9a69eb7 (stuff):week8/Lab/canvas.js
 }
 
 
