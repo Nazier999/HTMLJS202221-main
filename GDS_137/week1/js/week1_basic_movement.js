d3e089817a7f8a7eaf217abcce5d345e8dc6d567
Nazier999 <115019179+Nazier999@users.noreply.github.com>
2023-03-31 14:32:30 -0400

    week1

---
+++ GDS_137/week1/js/week1_basic_movement.js
@@ -0,0 +1,56 @@
+//Declare my variables
+
+var canvas;
+var context;
+var timer;
+//1000 ms or 1 second / FPS
+var interval = 1000/60;
+var player;
+
+	//Set Up the Canvas
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");	
+	
+	//Instantiate the Player
+	player = new Player();
+	player.width=100
+	player.height=100
+	
+	//Set the Animation Timer
+	timer = setInterval(animate, interval);
+
+function animate()
+{
+	//Erase the Screen
+	context.clearRect(0,0,canvas.width, canvas.height);	
+	
+	//Move the Player
+	player.x += 8;
+	
+	//Update the Screen
+	player.draw();
+
+	//Move the Player
+    player.move();
+
+    //collision
+    if(player.x < player.width/2){
+        player.x = player.width/2
+        player.vx = -player.vx;
+    }
+
+    if(player.x > canvas.width - player.width/2){
+        player.x = canvas.width - player.width/2
+        player.vx = -player.vx;
+    }
+    if(player.y < player.height/2){
+        player.y = player.height/2
+        player.vy = -player.vy;
+    }
+
+    if(player.y > canvas.height - player.height/2){
+        player.y = canvas.height - player.height/2
+        player.vy = -player.vy;
+    }
+
+}
