acacebbceb5a4fb6d7f8edd297b623d76ef263f0
Nazier999 <115019179+Nazier999@users.noreply.github.com>
2023-04-07 13:59:33 -0400

    1 player pong

--- GDS_137/week1/js/game.js
+++ GDS_137/week1/js/game.js
@@ -7,12 +7,12 @@
     
     var player1 = new GameObject();
     var ball = new GameObject();
-   player1.width = 25;
+   player1.width = 10;
    player1.height = 200;
-   player1.x = 64;
+   player1.x = 0;
    player1.y = canvas.height / 2;
    player1.color = "black";
-   player1.vy
+   player1.vy;
    ball.vx = 5
    ball.vy = 5
 
@@ -32,12 +32,12 @@
          if(s)
 	    {
 	    	console.log("Moving Down");
-	    	player1.y += 2;
+	    	player1.y += 4;
 	    }
 	     if(w)
 	    {
 	        console.log("Moving Up");
-	    	player1.y += -2;
+	    	player1.y += -4;
 	    }
 
         //player1 boundrys
@@ -52,11 +52,12 @@
         }
 
          //ball collision
-         if(ball.x < ball.width/2){
+
+        /*if(ball.x < ball.width/2){
             ball.x = ball.width/2
             ball.vx = -ball.vx;
             ball.color="green"
-        }
+        }*/
     
         if(ball.x > canvas.width - ball.width/2){
             ball.x = canvas.width - ball.width/2
@@ -79,6 +80,20 @@
         ball.drawCircle();
          //Move the Player
          ball.move();
+
+
+        
+	
+
+   	//Impede movement
+	if(ball.hitTestObject(player1))
+	{
+		//ball.x = player1.x - ball.width/2;
+        ball.x = ball.width/2
+            ball.vx = -ball.vx;
+            ball.color="green"
+		console.log("colliding");
+	}
         //context.clearRect(0,0,canvas.width, canvas.height);	
     }
     
