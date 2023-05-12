fedb9c3007c8505275576eeb47be12d8558d6d06
Nazier999 <115019179+Nazier999@users.noreply.github.com>
2023-05-12 12:40:28 -0400

    squares

--- GDS_137/week5_Arrays/js/week5_Arrays.js
+++ GDS_137/week5_Arrays/js/week5_Arrays.js
@@ -17,7 +17,7 @@ colors[0] = "#ff0000";
 colors[1] = "#00ff00";
 colors[2] = "#0000ff";
 
-var amt = 1;	
+var amt = 50;	
 var dots = [];
 
 for(var i = 0; i < amt; i++)
@@ -25,31 +25,22 @@ for(var i = 0; i < amt; i++)
 	dots[i] = new GameObject();
 	dots[i].x = Math.random() * canvas.width;
 	dots[i].y = Math.random() * canvas.height;
-	dots[i].width = rand(120,120);
+	dots[i].width = rand(5,20);
 	dots[i].vy = rand(-40,40);
 	dots[i].vx = rand(-10,10);
 	dots[i].color = colors[Math.floor(rand(0,2.9))];
 }
 
-var states = [];
-var currentState = 0;
-
-states['square'] = function(){dots[i].drawRect()}
-states['circle'] = function(){dots[i].drawCircle()}
-
 function animate()
 {
 	context.clearRect(0,0,canvas.width, canvas.height);	
 	
-	states[currentState]()
-
-
-	/*for(var i = 0; i < dots.length; i++)
+	for(var i = 0; i < amt; i++)
 	{
 		dots[i].drawCircle();
 		//dots[i].vy += gravity;
 		//dots[i].move();
-	}*/
+	}
 }
 
 
