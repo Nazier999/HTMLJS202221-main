8087a846c820e941d7ee73790e31e8b42e73114e
Nazier999 <115019179+Nazier999@users.noreply.github.com>
2023-05-19 13:37:40 -0400

    yippeeee

--- GDS_137/js/trig.js
+++ GDS_137/js/trig.js
@@ -46,11 +46,11 @@ function animate()
 	
 	/*-----------These move the follower-----------------*/
 	//magnet(); //- eases the follower towards the player - 
-	//point(); //- points at the player
-	//follow(); //- follows the player
+	point(); //- points at the player
+	follow(); //- follows the player
 	//orbit(); //- orbits the player using physics
 	//revolve(); //- orbits the player without physics.
-	//sinWave(); //- moves the follower in a sin wave pattern from left to right
+	sinWave(); //- moves the follower in a sin wave pattern from left to right
 	
 	ret.move
 
